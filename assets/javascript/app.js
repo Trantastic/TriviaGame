68e54e94862e4029fd3e8f19165b183108be894f
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-25 19:46:11 -0800

    Created array object of triviaBank. Timer works and questions cycle through

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,70 @@
-/*
-Create a START page
-	<button> to trigger start of the game
-
-
-		Each question triggers 15sec timer
-		Radio buttons for choices A-D
-			If/else statements
-				if === correct answer
-					points++
-					call function to show congratulating page
-						call timer to trigger next question after 5sec
-				
-				else if player runs out of time
-					call function to show player is wrong and correct answer
-						call timer to trigger next question after 5sec
-				else
-					wrong++
-					call function to show player is wrong and correct answer
-						call timer to trigger next question after 5sec
-		End of loop trigger endOfGame function
-			Display:
-				You got x out of x correct
-				Answers correct: x
-				Answers wrong: x
-				Do you want to play again?
-					<button> to trigger loop function without
-					refreshing page
-
-
-*/
 var correct;
 var wrong;
-var count = 1;
-var timer = 5;
-var questionCycle;
+var count = 0;
+var timer = 11;
 
-var questionBank = ["Which famous Pomeranian (proclaimed as the World’s Cutest Dog) has over 17 million Facebook likes?", 
-"What is the term when a doggo stretches out on their belly with their back paws out?", 
-"Contrary to the myth, dogs can see in color. Which shades of color can dogs see?", 
-"What family friendly Disney movie depicts 2 dogs and a cat on an incredible journey through the wilderness back to their owners in San Francisco?", 
-"Which breed of dog is the most popular in the United States?", 
-"Popular in gifs, where do you “boop” a dog?", 
-"Which internet term for dogs does not belong?", 
-"What popular twitter account popularized the phrase “they’re good dogs Brent” and has since exploded into a popular meme?", 
-"Popularly known as the doge meme, which breed of dog is depicted in this meme? (Hint: Most of the time this meme consists of just the dog’s head or facial expression with multi-colored text in Comic Sans)", 
-"Which fact is true about dogs?"];
-
-// Click events to start or restart game
-$("#start").on("click", startGame);
-$("#next").on("click", nextQuestion);
-
-
+// Objects in an array. Holds questions, answers, and options
+var triviaBank = 
+[{"question": "Which famous Pomeranian (proclaimed as the World’s Cutest Dog) has over 17 million Facebook likes?",
+"answer": "Boo",
+"options": ["Boo", "Toto", "Lassie", "Bobo"]},
+{"question": "What is the term when a doggo stretches out on their belly with their back paws out?",
+"answer": "Sploot",
+"options": ["Toot", "Scoot", "Sploot", "Chilling"]},
+{"question": "Contrary to the myth, dogs can see in color. Which shades of color can dogs see?",
+"answer": "Blue, yellow, and green",
+"options": ["Blue and red", "Only blue", "Blue, yellow, and red", "Blue, yellow, and green"]},
+{"question": "What family friendly Disney movie depicts 2 dogs and a cat on an incredible journey through the wilderness back to their owners in San Francisco?",
+"answer": "Homeward Bound",
+"options": ["The Incredible Journey", "Homeward Bound", "The Lost Pets", "Cujo"]},
+{"question": "Which breed of dog is the most popular in the United States?",
+"answer": "Labrador Retriever",
+"options": ["Golden Retriever", "Labrador Retriever", "American Bulldog", "German Shepherd"]},
+{"question": "Popular in gifs, where do you “boop” a dog?",
+"answer": "Their nose",
+"options": ["Their eye", "Their paw", "Their nose", "Their butt"]},
+{"question": "Which internet term for dogs does not belong?",
+"answer": "Purrito",
+"options": ["Pupperino", "Fluffers", "Loaf", "Purrito"]},
+{"question": "What popular twitter account popularized the phrase “they’re good dogs Brent” and has since exploded into a popular meme?",
+"answer": "WeRateDogs",
+"options": ["WeRateDogs", "DogsAreAwesome", "DogWatcher", "ThoughtsOfDog"]},
+{"question": "Popularly known as the doge meme, which breed of dog is depicted in this meme? (Hint: Most of the time this meme consists of just the dog’s head or facial expression with multi-colored text in Comic Sans)",
+"answer": "Shiba Inu",
+"options": ["Golden Retriever", "Chihuahua"," Siberian Husky", "Shiba Inu"]},
+{"question": "Which fact is true about dogs?",
+"answer": "All of the above.",
+"options": ["They are a Hooman’s best friend.", "Are better than cats.", "Have evolved to recognize pointing and reading facial expressions.", "All of the above."]}];
+startGame();
+// Creates the start button, which triggers game start
 function startGame(){
-	$("#questions").html("<div>" + questionBank[0] + "</div>");
-	var questionCycle = setInterval(nextQuestion, 1000 * 5);
-	countDown();
+	$("#start").html("<button>Click to Start</button>");
+	$("#start").on("click", function(){
+		//Erases start button when trivia starts
+		$("#start").empty();
+		$("#questions").html("<div>" + triviaBank[count].question + "</div>");
+		// New question every 15 sec
+		var questionCycle = setInterval(nextQuestion, 1000 * 11);
+
+		count++;
+
+		countDown();
+
+			// This will display options dependant on var count
+			$("#options").append("<button>" + triviaBank[count].options[i] + "</button>");		
+	});
 }
 
 function nextQuestion(){
-	$("#questions").html("<div>" + questionBank[count] + "</div>");
+	console.log(count);
 	count++;
+	
+
+	if(timer === 0){
+		timer = 11;
+		countDown();
+	}
+	$("#questions").html("<div>" + triviaBank[count].question + "</div>");
 
 	// if(count === 10){
 	// clearInterval(questionCycle);
@@ -73,16 +78,17 @@ function countDown(){
 	timer--;
 	console.log(timer);
 	
+	// setInterval(countDown, 1000);
 	if(timer > 0){
 		setTimeout(countDown , 1000);
 		// call function to show gif/image page
 	}
 	// if timer runs out go on to next page
-	else if(timer === 0){
-		// call function to show gif/image page
-		timer = 5;
-		countDown();
-	}
+	// if(timer === 0){
+	// 	// call function to show gif/image page
+	// 	timer = 5;
+	// 	// countDown();
+	// }
 
 	$("#count-down").html(timer);
 }
